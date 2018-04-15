8fa46bcf96e02800a7fb52d587676560e59e176e
Daniel Bohannon <dhbohannon@gmail.com>
2018-04-15 18:40:16 -0400

    Create TextFunctions.ps1

---
+++ PSScriptAnalyzer/Samples/Obfuscated/InvokeObfuscation/TextFunctions.ps1
@@ -0,0 +1,9 @@
+Function NEw-`li`Ne([string]${stri`N`GIn})
+{
+ "-" * ${stR`iN`g`iN}."L`En`GTH"
+} 
+
+Function G`et-teX`TsTats([string[]]${Te`x`TiN})
+{
+ ${t`Ext`iN} | .("{0}{2}{1}{3}{4}" -f 'M','re','easu','-O','bject') -Line -word -char
+} 
